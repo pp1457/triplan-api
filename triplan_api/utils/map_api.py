a5554af9c867c3593f9ad327898a221b1ba729de
unknown <leo.ho7410@gmail.com>
2024-12-05 22:15:59 +0800

    translate comment

--- triplan_api/utils/map_api.py
+++ triplan_api/utils/map_api.py
@@ -2,12 +2,13 @@ import requests
 
 def text_search(text_query, latitude, longitude, api_key):
     """
-    呼叫 Google Maps Places API 的 Text Search。
+    Calls the Google Maps Places API Text Search.
 
-    :param text_query: 查詢文字 (如 "Spicy Vegetarian Food in Sydney, Australia")
-    :param latitude: 中心點的緯度
-    :param longitude: 中心點的經度
-    :return: JSON (至多 20 筆的 place_id)
+    :param text_query: Query text (e.g., "Spicy Vegetarian Food in Sydney, Australia")
+    :param latitude: Latitude of the central point
+    :param longitude: Longitude of the central point
+    :param api_key: Google Maps API key
+    :return: JSON (up to 20 place_id results)
     """
     url = 'https://places.googleapis.com/v1/places:searchText'
     headers = {
@@ -33,19 +34,20 @@ def text_search(text_query, latitude, longitude, api_key):
     return response.json()
 
 '''
-locationBias: 盡量回傳在其中的地點，圓心為經緯度，半徑單位為公尺
-有需要超過20筆結果再加pageToken
-預設照關聯性排序，有需要照距離排序再加rankPreference
+locationBias: Tries to return locations within the specified area, with the center defined by latitude and longitude, and radius in meters.
+Use pageToken if more than 20 results are needed.
+By default, results are sorted by relevance. To sort by distance, add rankPreference.
 '''
 
 ##############################################################
 
 def place_details(place_id, api_key):
     """
-    呼叫 Google Maps Places API 的 Place Details。
+    Calls the Google Maps Places API Place Details.
 
-    :param place_id: 地點的唯一 ID
-    :return: JSON (類型、地址、評分、網址、評分數、名字、簡介、至多五則留言)
+    :param place_id: Unique ID of the location
+    :param api_key: Google Maps API key
+    :return: JSON (type, address, rating, URL, review count, name, description, up to five reviews)
     """
     url = f'https://places.googleapis.com/v1/places/{place_id}?languageCode=zh-TW'
     headers = {
@@ -61,13 +63,13 @@ def place_details(place_id, api_key):
 
 def routes(origin_place_id, destination_place_id, travel_mode, api_key):
     """
-    呼叫 Google Maps Routes API。
+    Calls the Google Maps Routes API.
     
-    :param origin_place_id: 起點的 Place ID
-    :param destination_place_id: 終點的 Place ID
-    :param travel_mode: 交通方式（"DRIVE", "BICYCLE", "WALK", "TRANSIT", "TWO_WHEELER"）
-    :param api_key: Google Maps API 金鑰
-    :return: JSON (時間 秒、距離 公尺)
+    :param origin_place_id: Place ID of the starting location
+    :param destination_place_id: Place ID of the destination
+    :param travel_mode: Mode of transportation ("DRIVE", "BICYCLE", "WALK", "TRANSIT", "TWO_WHEELER")
+    :param api_key: Google Maps API key
+    :return: JSON (time in seconds, distance in meters)
     """
     url = 'https://routes.googleapis.com/directions/v2:computeRoutes?languageCode=zh-TW'
     headers = {
